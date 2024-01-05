b26f6de443933e62de750de253f53ce8caa5670f
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2024-01-05 08:36:33 +0000

    Add profilelist view and path

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    orders profile list by creation in descending order
+    """
+    queryset = Profile.objects.all().order_by('-created_at')
+    
